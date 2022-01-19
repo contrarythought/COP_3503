78a481f5ac9dfcab306682f80207e6992dc01f50
Anthony Thorpe <59274215+contrarythought@users.noreply.github.com>
2022-01-19 18:49:52 -0500

    Create tentaizu.java

---
+++ p2/tentaizu.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class tentaizu {
+    private String[][] board;
+    private final int NUM_ROWS = 7;
+    private final int NUM_COL = 7;
+    private int numBoards;
+    public static void main(String[] args) throws IOException{
+        tentaizu t = new tentaizu();
+    }
+
+    // read in and generate the finished tentaizu boards
+    public tentaizu() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String numB = reader.readLine();
+        numBoards = Integer.parseInt(numB);
+
+        // loop through each board
+        for(int i = 0; i < numBoards; i++) {
+
+            board = new String[NUM_ROWS][NUM_COL];
+
+            for(int j = 0; j < NUM_ROWS; j++) {
+
+                String[] tmp = reader.readLine().split("");
+                board[j] = tmp;
+
+            }
+
+            // read in the separator
+            String sep = reader.readLine();
+
+        }
+
+    }
+
+    public void printBoard() {
+        for(int i = 0; i < board.length; i++) {
+
+            for(int j = 0; j < board[i].length; j++) {
+
+                System.out.print(board[i][j]);
+
+            }
+
+            System.out.println();
+
+        }
+    }
+}
