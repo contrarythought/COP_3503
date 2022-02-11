2e9aa94d8a1014e81a16fa46fb83c71758d5efa7
Anthony Thorpe <59274215+contrarythought@users.noreply.github.com>
2022-02-11 03:01:58 -0500

    Update dancerecital.java

--- Kattis/Dance Recital/dancerecital.java
+++ Kattis/Dance Recital/dancerecital.java
@@ -6,6 +6,7 @@ import java.util.PriorityQueue;
 
 public class dancerecital {
     public static PriorityQueue<Integer> pQueue;
+    public static boolean[] used;
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         
@@ -24,10 +25,30 @@ public class dancerecital {
         }
 
         pQueue = new PriorityQueue<Integer>();
-        int result = findMin(routines, numRoutines);
+        used = new boolean[numRoutines];
+        findMin(routines, new String[numRoutines], numRoutines, 0, 0);
+        int result = pQueue.poll();
     }
 
-    public static int findMin(ArrayList<ArrayList<String>> routines, int numRoutines) {
+    public static void findMin(ArrayList<ArrayList<String>> routines, String[] routPerm, int numRoutines, int k, int count) {
+        if(k == numRoutines) {
+            pQueue.add()
+        }
+        for(int i = 0; i < numRoutines; i++) {
+            if(used[k]) continue;
+            
+            if(k > 0 && routPerm[k - 1] != "") {
+                int z;
+                for(z = 0; z < Integer.max(routPerm[k - 1].length(), routines.get(k).size()); z++) {
 
+                }
+            }
+            for(int j = 0; j < routines.get(i).size(); j++) {
+                routPerm[j] = routines.get(i).get(j);
+            }
+
+            used[k] = true;
+
+        }
     }
 }
